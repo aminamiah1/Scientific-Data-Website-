487f1aa51aabadd41040c293c93abd3942e6a023
Valentine Wilson <wilsonvt@cardiff.ac.uk>
2023-11-08 14:03:18 +0000

    Initial routes and CI test script

---
+++ src/app/half-hourly/gas-boilers/page.tsx
@@ -0,0 +1,7 @@
+export default function GasBoilers() {
+    return (
+        <>
+            <h1>Gas Boiler content</h1>
+        </>
+    );
+}
---
+++ src/app/half-hourly/resistance-heaters/page.tsx
@@ -0,0 +1,7 @@
+export default function ResistanceHeaters() {
+    return (
+        <>
+            <h1>Resistance Heaters</h1>
+        </>
+    );
+}
