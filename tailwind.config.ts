c34ab3011d9f9b8a6fbf28a8b473268f04e31425
Mahi Sheraj <sherajmm@cardiff.ac.uk>
2023-11-11 23:40:24 +0000

    custom font class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        customBlue: 'rgba(1, 111, 160, 0.45)',
+      },
+
     },
   },
   plugins: [],
