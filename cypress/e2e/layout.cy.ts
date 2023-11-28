6624f5389fb5366ee9d522ede1105e45b27ac2dc
Joseph Taylor <taylorj51@cardiff.ac.uk>
2023-11-28 12:22:48 +0000

    Add ability to use data-cy html attr to access elements without using class/id selectors

--- cypress/e2e/layout.cy.ts
+++ cypress/e2e/layout.cy.ts
@@ -6,25 +6,25 @@ describe("Layout", () => {
   it("Should be able to navigate to every page", () => {
     // Sticking to best practises, as per:
     // https://docs.cypress.io/guides/references/best-practices#How-It-Works
-    cy.get('[data-cy="nav-heat-demand"]').click();
+    cy.getByTestId("nav-heat-demand").click();
     cy.url().should("include", "/heat-demand");
 
-    cy.get('[data-cy="nav-breakdown-heat"]').click();
+    cy.getByTestId("nav-breakdown-heat").click();
     cy.url().should("include", "/breakdown/heat");
 
-    cy.get('[data-cy="nav-breakdown-energy"]').click();
+    cy.getByTestId("nav-breakdown-energy").click();
     cy.url().should("include", "/breakdown/energy");
 
-    cy.get('[data-cy="nav-half-hourly-gas-boilers"]').click();
+    cy.getByTestId("nav-half-hourly-gas-boilers").click();
     cy.url().should("include", "/half-hourly/gas-boilers");
 
-    cy.get('[data-cy="nav-half-hourly-resistance-heaters"]').click();
+    cy.getByTestId("nav-half-hourly-resistance-heaters").click();
     cy.url().should("include", "/half-hourly/resistance-heaters");
 
-    cy.get('[data-cy="nav-login-sign-in"]').click();
+    cy.getByTestId("nav-login-sign-in").click();
     cy.url().should("include", "/admin/sign-in");
 
-    cy.get('[data-cy="nav-login-sign-up"]').click();
+    cy.getByTestId("nav-login-sign-up").click();
     cy.url().should("include", "/admin/sign-up");
   });
 });
