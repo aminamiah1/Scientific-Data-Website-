8bea43b502ab5fd0587fce79116c9d94519de327
Amina Miah <Miaha9@cardiff.ac.uk>
2023-12-13 18:51:40 +0000

    testing fix

--- cypress/e2e/sign-in.cy.ts
+++ cypress/e2e/sign-in.cy.ts
@@ -1,14 +1,21 @@
 describe("Admin Sign-In", () => {
-
   it("successfully signs out", () => {
     cy.visit("/api/auth/signout");
     cy.get("#submitButton").click();
+    cy.visit("/admin/add-admins");
+    cy.contains("You are not authorized to view this page!").should("exist");
   });
 
-  it('shows access denied when entering email', () => {
+  it("shows access denied when entering email", () => {
     cy.visit("/admin/sign-in");
-    cy.get('#email').type('user@example.com');
-    cy.get("#admin-login-form").submit();
-    cy.contains('Access Denied').should('exist');
+    cy.get("#email")
+      .type("wronguser@gmail.com")
+      .then(() => {
+        cy.get("#adminSubmit").click();
+      });
+    cy.url().should(
+      "equal",
+      "http://localhost:3000/api/auth/error?error=AccessDenied"
+    );
   });
 });
