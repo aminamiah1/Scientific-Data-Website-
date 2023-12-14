c43e25a7809d66e9100c711680563ed9b5bb4e66
Amina Miah <Miaha9@cardiff.ac.uk>
2023-12-14 17:40:44 +0000

    changed file name

--- src/app/admin/sign-in/page.tsx
+++ src/app/admin/sign-in/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import AuthContext from "@/app/utils/authContext";
-import SignInComponent from "@/app/components/signInComponent";
+import SignInComponent from "@/app/components/SignIn/SignInComponent";
 
 export default function SignIn() {
   return (
