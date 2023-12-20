e38c66cdefef93a604b272f8a3ea57d26c450784
Amina Miah <miaha9@cardiff.ac.uk>
2023-12-20 14:52:41 +0000

    Spec File Bug Fix

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: "qnxosg",
-  e2e: {
-    baseUrl: "http://localhost:3000",
-    specPattern: "cypress/e2e/*.{cy,spec}.{js,jsx,ts,tsx}",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    projectId: "qnxosg",
+    e2e: {
+        baseUrl: "http://localhost:3000",
+        specPattern: "cypress/e2e/*.{cy,spec}.{js,jsx,ts,tsx}",
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
 
-  component: {
-    specPattern: "cypress/{component,api}/*.{cy,spec}.{js,jsx,ts,tsx}",
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
+    component: {
+        // specPattern: "cypress/{component,api}/*.{cy,spec}.{js,jsx,ts,tsx}",
+        devServer: {
+            framework: "next",
+            bundler: "webpack",
+        },
     },
-  },
 });
